7f29d42738780c653acb7412803e7c81b8309edf
douglasrsa <97745559+douglasrsa@users.noreply.github.com>
2022-02-03 15:52:50 -0300

    Add files via upload

---
+++ Atividade Verificar palíndromo e substituir números pares/palindromo1.js
@@ -0,0 +1,9 @@
+//solução 1
+function verificarPalindromo(string) {
+    if (!string) return;
+
+    return string.split("").reverse().join("") === string;
+
+}
+
+console.log(verificarPalindromo("reviver"));
